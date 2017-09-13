15d3851c8184caed9b7833aba67f455341e5fc8f
Zain Wania <zainthegreatcb@gmail.com>
2017-09-13 11:11:47 -0400

    added some basic tests and small syntax consistancy

--- spec/pubg_rb_spec.rb
+++ spec/pubg_rb_spec.rb
@@ -1,11 +1,22 @@
 require "spec_helper"
+require "yaml"
 
 RSpec.describe PubgRb do
+  before(:each) do
+    CONFIG = OpenStruct.new YAML.load_file 'spec/secrets.yaml'
+    @api_key= CONFIG["APIKEY"]
+  end
+
   it "has a version number" do
     expect(PubgRb::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "raises an ArgumentError when no API key is passed" do
+    expect{PubgRb::Api.new()}.to raise_error(ArgumentError, "Add an `api_key` from `pubgtracker.com`!")
+  end
+
+  #TODO validate API key somehow
+  it "Initializes client successfully" do
+    expect(PubgRb::Api.new(@api_key)).to be_a PubgRb::Api
   end
 end
