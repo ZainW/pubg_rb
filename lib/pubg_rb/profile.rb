ce8ba30b5e766e3de78d9cb9e7053a2d68fdbdfa
Zain Wania <zainthegreatcb@gmail.com>
2017-09-18 13:24:20 -0400

    lowercase vars for profile and manually dirty convert DateTime

--- lib/pubg_rb/profile.rb
+++ lib/pubg_rb/profile.rb
@@ -2,7 +2,8 @@ module PubgRb
   class Profile
     attr_reader :platformid, :accountid, :avatar, :selectedregion, :defaultseason, :seasondisplay, :lastupdated, :playername, :pubgtrackerid, :stats, :matchhistory
     def initialize(opts={})
-      opts.each { |k,v| instance_variable_set("@#{k}", v) }
+      opts.each { |k,v| instance_variable_set("@#{k.downcase}", v) }
+      instance_variable_set("@lastupdated", DateTime.parse(self.lastupdated[0..18]))
     end
   end
 end
